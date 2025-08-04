48066af3690ee7136f77a0ebda385951104540f5
Júlio <213834242+JulioAlencar0@users.noreply.github.com>
2025-08-04 19:52:47 -0300

    add router get resumo

--- src/routes/rotasTransacao.js
+++ src/routes/rotasTransacao.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {getTransacoes, createTransacoes, getResumoFinanceiro} = require('./../controllers/controleTransacao')
 
+router.get('/resumo', getResumoFinanceiro)
 router.get('/', getTransacoes)
 router.get('/:id', getTransacoes)
 router.post('/',createTransacoes )
-router.get('/resumo', getResumoFinanceiro)
 module.exports = router
