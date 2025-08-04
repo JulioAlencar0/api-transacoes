c5ad03cd2eef34efab70be3ef4c030f3f560c877
Júlio <213834242+JulioAlencar0@users.noreply.github.com>
2025-08-04 19:50:25 -0300

    add router get :id

--- src/routes/rotasTransacao.js
+++ src/routes/rotasTransacao.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {getTransacoes, createTransacoes, getResumoFinanceiro} = require('./../controllers/controleTransacao')
 
 router.get('/', getTransacoes)
+router.get('/:id', getTransacoes)
 router.post('/',createTransacoes )
 router.get('/resumo', getResumoFinanceiro)
 module.exports = router
